4c52adca2fde07141830a31e5491b8efc06059bb
Charles Averill <charlesaverill20@gmail.com>
2023-11-16 18:33:11 -0600

    Need to add more replacements

--- generate.sh
+++ generate.sh
@@ -1,9 +1,11 @@
-cd mf # https://www.ctan.org/tex-archive/fonts/cm/mf
+TEXENGINE=pdflatex
 
+FONTSIZE=300
 PTSIZE=12
 PRECISION=10
 
-CHARS=$(tr -d '[:space:]' < ../letter.txt | sed -e 's/\(.\)/\1/g')
+RAWCHARS=$(cat contents.txt)
+CHARS=$(tr -d '[:space:]' < contents.txt | sed -e 's/\(.\)/\1/g')
 N_CHARS=${#CHARS}
 
 X_HEIGHT_MIN="186/36"
@@ -29,6 +31,23 @@ function lerp { # lerp a b t
 	echo "scale=${PRECISION}; $1 + ($2 - $1) * $3" | bc
 }
 
+number_to_string() {
+  local num=$1
+  local result=""
+
+  while ((num >= 0)); do
+    ((remainder = num % 26))
+    result=$(printf \\$(printf '%03o' $((97 + remainder))))$result
+    ((num = (num / 26) - 1))
+  done
+
+  echo "$result"
+}
+
+cd mf # https://www.ctan.org/tex-archive/fonts/cm/mf
+
+FONT_DECLS=""
+
 for (( i=0; i<${#CHARS}; i++ )); do
   cp "cmr${PTSIZE}.mf" temp.mf
   t=$(lerp 0 1 "1 - (${#CHARS}-$i)/${#CHARS}")
@@ -37,9 +56,50 @@ for (( i=0; i<${#CHARS}; i++ )); do
   # Do replacements
   sed -i "s%x_height#:=186/36pt#;%x_height#:=$(lerp $X_HEIGHT_MIN $X_HEIGHT_MAX $t)pt#;%g" temp.mf
 
-  mf -halt-on-error temp.mf
-  mv temp.2602gf ../generated_fonts/"$i.2602gf"
-  rm temp.mf
+  # Compile
+  mf -halt-on-error '\mode=cx;' input temp.mf
+
+  # Copy fonts out
+  mv temp.tfm ../"font$i.tfm"
+  gftopk "temp.${FONTSIZE}gf"
+  mv "temp.${FONTSIZE}pk" ../"font$i.${FONTSIZE}pk"
+
+  NUMLET=$(number_to_string $i)
+  FONT_DECLS+="\newfont{\font$NUMLET}{out/font$i}"
 done
 
 cd ..
+
+rm mf/temp*
+
+# font declaration replacements
+cp main.tex temp.tex
+ESCAPED_REPLACE=$(printf '%s\n' "$FONT_DECLS" | sed -e 's/[\/&]/\\&/g')
+sed -i "s#FONTDECLS#$ESCAPED_REPLACE#g" temp.tex
+
+# content replacements
+TO_INSERT=""
+n="0"
+for (( i=0; i < ${#RAWCHARS}; i++ )); do
+  case ${RAWCHARS:$i:1} in 
+    [[:space:]])
+        TO_INSERT+="${RAWCHARS:$i:1}"
+        continue
+      ;;
+  esac
+  NUMLET=$(number_to_string $n)
+  TO_INSERT+="\font$NUMLET{}${CHARS:$n:1}"
+  
+  n=$(echo "$n + 1" | bc);
+done
+ESCAPED_REPLACE=$(printf '%s\n' "$TO_INSERT" | sed -e 's/[\/&]/\\&/g')
+sed -i "s#CONTENTS#$ESCAPED_REPLACE#g" temp.tex
+
+cat temp.tex
+
+$TEXENGINE temp.tex
+
+# Cleanup
+rm temp.tex
+rm *300pk *.tfm
+mv temp.pdf main.pdf
